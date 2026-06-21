792d5d60cecfa60317278fe4c3223d5b0347de96
Leonid <lenh4rdt@gmail.com>
2026-06-21 23:17:30 +0300

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   output: 'server',
+
   devToolbar: {
     enabled: false
   },
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   adapter: node({
     mode: 'standalone'
   }),
-  
+
+  integrations: [sitemap()],
 }
 );
